f5c7b253d417f697e66d8c9423753a5472292ef8
busayo <prosperotemuyiwa@gmail.com>
2015-11-04 04:16:47 +0200

    Add simple placeholder test

---
+++ tests/LaravelFeederTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Unicodeveloper\LaravelFeeder\Test;
+
+class LaravelFeederTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
